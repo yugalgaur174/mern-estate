130cb2854daf390b0e569f7124e9b76a440a1730
YUGAL GAUR <yugalgaur174@gmail.com>
2025-01-04 23:17:43 +0530

    Backend is Setup

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Mongo db connected")
+}).catch((err)=>{
+    console.log("Error: "+err);
+})
+
+
+
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000!?!");
+})
