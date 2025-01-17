8f94c88e6e7d6b7b5d572766179c3b2ca94a0438
YUGAL GAUR <yugalgaur174@gmail.com>
2025-01-17 10:52:03 +0530

    Profile page built

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,29 +1,57 @@
-import React from 'react'
-import {FaSearch} from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <header className='bg-slate-300 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to="/">
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>CodingCleverly</span>
-                <span className='text-slate-700'>Estate</span>
-            </h1>
-            </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent  focus:outline-none w-24 sm:w-64' />
-            <FaSearch className='text-slate-600'></FaSearch>
+    <header className="bg-slate-300 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">CodingCleverly</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent  focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600"></FaSearch>
         </form>
-        <ul className='flex gap-4'>
-
-            <Link to="/"><li className=' text-slate-700 sm:inline hidden hover:underline'>Home</li></Link>
-            <Link to="/about"><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-            <Link to="/signin"><li className='sm:inline text-slate-700 hover:underline'>Sign In</li></Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className=" text-slate-700 sm:inline hidden hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          {/* <Link to="/signin"><li className='sm:inline text-slate-700 hover:underline'>Sign In</li></Link> */}
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="avatar"
+                className="w-8 h-8 rounded-full object-cover"
+              ></img>
+            ) : (
+              <li className="sm:inline text-slate-700 hover:underline">
+                Sign In
+              </li>
+            )}
+          </Link>
         </ul>
-        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>
-        Profile
-    </div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+      <form action="" className="flex flex-col gap-5 ">
+        <img
+          src={currentUser.avatar}
+          alt="avatar"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-3"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          defaultValue={currentUser.username}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          defaultValue={currentUser.password}
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-90">
+          Submit
+        </button>
+      </form>
 
-export default Profile
+      <div className="flex justify-between mt-5 ">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
